9982a354bca27c7693941f52938d658327221668
PAVANKUMAR <PAVANKUMAR@PAVAN>
2019-08-08 20:22:21 +0530

    Added Text

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,11 +8,11 @@ namespace WebApplication1.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
+		public ActionResult Index()  
 		{
 			return View();
 		}
-
+		 
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
